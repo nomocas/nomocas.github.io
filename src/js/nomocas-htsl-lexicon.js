16a858f50c4f2e1ec0972478b8b6d4bfab3e16e2
Gilles Coomans <gilles.coomans@gmail.com>
2017-04-26 17:35:04 +0200

    feat(stats): add google analytics

--- src/js/nomocas-htsl-lexicon.js
+++ src/js/nomocas-htsl-lexicon.js
@@ -21,6 +21,7 @@ module.exports = htslLexicon.createDialect('nomocas-htsl')
 							.pageHeader(),
 							content
 						)
+						.ga('UA-63787213-1')
 					);
 			},
 			pageHead() {
@@ -97,6 +98,27 @@ module.exports = htslLexicon.createDialect('nomocas-htsl')
 					.div(h.class('badges').badges('nomocas', name, true)),
 					sublibs
 				);
+			},
+			ga(key) {
+				return this.tag('script', [
+					h.onString((descriptor) => {
+						descriptor.children = `
+	(function(i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function() {
+			(i[r].q = i[r].q || []).push(arguments)
+		}, i[r].l = 1 * new Date();
+		a = s.createElement(o),
+			m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+	ga('create', '${ key }', 'auto');
+	ga('send', 'pageview');
+`;
+					})
+				]);
 			}
 		};
 	});
